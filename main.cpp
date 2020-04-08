64395d10b587291b00f1732610d7f7e5bb1fe605
S0mas <sommerfeld.krzysztof@gmail.com>
2020-04-08 16:53:45 +0200

    add fat mechanism to switch from MOCK to real app version: 1.Comment out line: #define MOCK 2.Specify ip address of 6991 device. (variable ip6991)

--- main.cpp
+++ main.cpp
@@ -4,21 +4,37 @@
 #include "../WizardFramework/Common/include/Device6991/gui/Controller6991.h"
 #include "../WizardFramework/Common/include/Device6991/gui/RegisterController.h"
 #include "../WizardFramework/Common/include/Device6991/Device6991.h"
+
+#define MOCK // needs to be commented in real app
+
+#ifdef MOCK
 #include "../WizardFramework/Mock/include/HardwareConnectorMock.h"
 #include "../WizardFramework/Common/include/Device6991/ScpiIF6991Mock.h"
+#elif
+#include "../WizardFramework/Common/include/Device6991/HardwareConnector6991.h"
+#include "../WizardFramework/Common/include/ScpiIFVisa.h" 
+#endif
 
 int main(int argc, char *argv[]) {
 	QApplication a(argc, argv);
 	QCoreApplication::setOrganizationName("Bustec");
 	QCoreApplication::setOrganizationDomain("Bustec");
 	QCoreApplication::setApplicationName("6991_TEST");
-
-	auto connector1 = new HardwareConnectorMock("MockConnector1");
-	QSettings().setValue("ipMockConnector1", "127.0.0.1");
-	auto connector2 = new HardwareConnectorMock("MockConnector2");
-	QSettings().setValue("ipMockConnector2", "127.0.0.1");
-
+	QString ip6991 = "127.0.0.1"; // 6991 device ip - in mock version it should be localhost
+	QString connectorName1 = "connector1";
+	QString connectorName2 = "connector2";
+#ifdef MOCK
+	auto connector1 = new HardwareConnectorMock(connectorName1); 
+	auto connector2 = new HardwareConnectorMock(connectorName2);
 	auto scpiIF = new ScpiIF6991Mock;
+#elif
+	auto connector2 = new HardwareConnector6991(connectorName1);
+	auto connector2 = new HardwareConnector6991(connectorName2);
+	auto scpiIF = new ScpiIFVisa;
+#endif
+	QSettings().setValue(connectorName1, ip6991);
+	QSettings().setValue(connectorName2, ip6991);
+
 	Device6991 dev("Device6111", connector1, scpiIF, 256);
 
 	auto controller6991 = new Controller6991(connector1, scpiIF, 1);
