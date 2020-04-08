846807d929d7e6fe25d9c1c318d9a8da6fca002f
S0mas <sommerfeld.krzysztof@gmail.com>
2020-04-08 16:36:16 +0200

    moved server implementation to 6991 device mock

--- main.cpp
+++ main.cpp
@@ -5,20 +5,7 @@
 #include "../WizardFramework/Common/include/Device6991/gui/RegisterController.h"
 #include "../WizardFramework/Common/include/Device6991/Device6991.h"
 #include "../WizardFramework/Mock/include/HardwareConnectorMock.h"
-#include <QTcpServer>
-
-QTcpSocket *clientConnection = nullptr;
-
-void write(QTcpServer* server) {
-	QByteArray block;
-	QDataStream out(&block, QIODevice::WriteOnly);
-	out.setVersion(QDataStream::Qt_5_14);
-	out << QString("DUPA");
-	if (!clientConnection)
-		clientConnection = server->nextPendingConnection();
-
-	clientConnection->write(block);
-}
+#include "../WizardFramework/Common/include/Device6991/ScpiIF6991Mock.h"
 
 int main(int argc, char *argv[]) {
 	QApplication a(argc, argv);
@@ -26,35 +13,25 @@ int main(int argc, char *argv[]) {
 	QCoreApplication::setOrganizationDomain("Bustec");
 	QCoreApplication::setApplicationName("6991_TEST");
 
-	auto server = new QTcpServer(&a);
-	server->listen();
-	qDebug() << QHostAddress(QHostAddress::LocalHost).toString() << ":" << server->serverPort();
-
 	auto connector1 = new HardwareConnectorMock("MockConnector1");
 	QSettings().setValue("ipMockConnector1", "127.0.0.1");
 	auto connector2 = new HardwareConnectorMock("MockConnector2");
 	QSettings().setValue("ipMockConnector2", "127.0.0.1");
 
-	auto writeButton = new QPushButton("write");
-	QObject::connect(writeButton, &QPushButton::clicked, [server]() { write(server); });
-	writeButton->show();
+	auto scpiIF = new ScpiIF6991Mock;
+	Device6991 dev("Device6111", connector1, scpiIF, 256);
 
-
-	auto device = new AbstractDeviceXX;
-	auto device2 = new AbstractDeviceXX;
-
-
-	auto scpiIF = new ScpiIFMock;
-
-	auto controller6991 = new Controller6991(connector1, scpiIF, server->serverPort());
-	auto controller6991_2 = new Controller6991(connector2, scpiIF, server->serverPort());
-	auto resgisterController = new RegisterController(device, device2);
-	auto testController = new TestsView(device);
+	auto controller6991 = new Controller6991(connector1, scpiIF, 1);
+	auto controller6991_2 = new Controller6991(connector2, scpiIF, 2);
+	auto resgisterControllerFrontend = new RegisterControllerFrontend(connector1, scpiIF);
+	auto resgisterController6991 = new RegisterController6991(connector1, scpiIF);
+	auto testController = new TestsView(connector1, scpiIF);
 
 	controller6991->show();
 	controller6991_2->show();
-	//resgisterController->show();
-	//testController->show();
+	resgisterControllerFrontend->show();
+	resgisterController6991->show();
+	testController->show();
 
 	return a.exec();
 }
