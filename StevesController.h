ba290819899247aac9e63eddf8cd8d552916f5a3
S0mas <sommerfeld.krzysztof@gmail.com>
2020-04-30 11:27:29 +0100

    Steves program + fixes

---
+++ StevesController.h
@@ -0,0 +1,212 @@
+#pragma once
+
+#include <QWidget>
+#include <QCheckBox>
+#include <QComboBox>
+#include <QGroupBox>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QTimer>
+#include <QMessageBox>
+#include <QLabel>
+#include "../WizardFramework/Common/include/Device6132.h"
+
+enum ConfigAction {
+	SET_GAIN,
+	SET_FILTER
+};
+
+enum Target {
+	ALL = 0
+};
+
+class StevesController : public QWidget {
+	Q_OBJECT
+	Device6132* devIF_;
+	QPushButton* measureButton_ = new QPushButton("Single Conversion");
+	QPushButton* startStopAcqButton_ = new QPushButton("Continous Conversion");
+	QGroupBox* setterGroup_ = new QGroupBox("Configure");
+	QTimer* acqTimer_ = new QTimer(this);
+	std::vector<bool> enabledChannels_;
+	std::vector<uint32_t> allChannels = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+
+	void doMeasurement() {
+		std::vector<uint32_t> channels;
+		for (auto chId : allChannels)
+			if (enabledChannels_[chId - 1])
+				channels.push_back(chId);
+		if (auto result = devIF_->measure(channels); result)
+			emit measurementUpdate(*result);
+	}
+
+	void fetchConfig() {
+		std::vector<std::optional<GainType6132::Type>> gains;
+		for (auto channel : allChannels)
+			gains.push_back(devIF_->gain(channel));
+		emit gainsUpdate(gains);
+		std::vector<std::optional<FilterType6132::Type>> filters;
+		for (auto channel : allChannels)
+			filters.push_back(devIF_->filter(channel));
+		emit filtersUpdate(filters);
+	}
+signals:
+	void measurementUpdate(std::vector<std::optional<double>> const& values);
+	void gainsUpdate(std::vector<std::optional<GainType6132::Type>> const& values);
+	void filtersUpdate(std::vector<std::optional<FilterType6132::Type>> const& values);
+public:
+	StevesController(Device6132* devIF, QWidget* parent = nullptr) : QWidget(parent), devIF_(devIF) {
+		connect(measureButton_, &QPushButton::clicked, this, &StevesController::doMeasurement);
+		connect(acqTimer_, &QTimer::timeout, this, &StevesController::doMeasurement);
+		connect(startStopAcqButton_, &QPushButton::clicked, 
+			[this]() {
+				if (startStopAcqButton_->text() == "Continous Conversion") {
+					fetchConfig();
+					std::vector<uint32_t> channels;
+					for (auto chId : allChannels)
+						if (enabledChannels_[chId - 1])
+							channels.push_back(chId);
+					if (!channels.empty()) {
+						acqTimer_->start(100);
+						startStopAcqButton_->setText("Stop");
+						measureButton_->setDisabled(true);
+						setterGroup_->setDisabled(true);
+					}
+				}
+				else {
+					acqTimer_->stop();
+					startStopAcqButton_->setText("Continous Conversion");
+					measureButton_->setEnabled(true);
+					setterGroup_->setEnabled(true);
+				}
+			}
+		);
+
+		auto comboBoxAction = new QComboBox;
+		comboBoxAction->addItem("SET_GAIN", SET_GAIN);
+		comboBoxAction->addItem("SET_FILTER", SET_FILTER);
+		auto comboBoxValue = new QComboBox;
+		auto comboBoxTarget = new QComboBox;
+		comboBoxTarget->addItem("ALL_CHANNELS", 0);
+		for (auto channel : allChannels)
+			comboBoxTarget->addItem(QString("%1").arg(channel), channel);
+		auto setButton = new QPushButton("Set");
+
+		auto hlayout = new QHBoxLayout;
+		hlayout->addWidget(comboBoxAction);
+		hlayout->addWidget(comboBoxValue);
+		hlayout->addWidget(comboBoxTarget);
+		hlayout->addWidget(setButton);
+		setterGroup_->setLayout(hlayout);
+
+		connect(comboBoxAction, QOverload<QString const&>::of(&QComboBox::currentIndexChanged),
+			[comboBoxValue](QString const& action) {
+				comboBoxValue->clear();
+				if (action == "SET_GAIN") 
+					for (auto gain : GainType6132::TYPES)
+						comboBoxValue->addItem(GainType6132::toString(gain), gain);
+				if (action == "SET_FILTER")
+					for (auto filter : FilterType6132::TYPES)
+						comboBoxValue->addItem(FilterType6132::toString(filter), filter);			
+			}
+		);
+		emit comboBoxAction->currentIndexChanged(comboBoxAction->currentText());
+
+		connect(setButton, &QPushButton::clicked,
+			[comboBoxAction, comboBoxValue, comboBoxTarget, this]() {
+				std::vector<uint32_t> channels = comboBoxTarget->currentData().toUInt() == 0 ? allChannels : std::vector<uint32_t>{comboBoxTarget->currentData().toUInt()};
+				bool result = false;
+				if (comboBoxAction->currentData().toUInt() == SET_GAIN)
+					result = devIF_->setGain(channels, static_cast<GainType6132::Type>(comboBoxValue->currentData().toUInt()));
+				else if(comboBoxAction->currentData().toUInt() == SET_FILTER) 
+					result = devIF_->setFilter(channels, static_cast<FilterType6132::Type>(comboBoxValue->currentData().toUInt()));
+				if (!result)
+					QMessageBox::critical(this, "Error", QString("Faild to %1.").arg(comboBoxAction->currentText()));
+				else
+					fetchConfig();
+			}
+		);
+		auto checkAllButton = new QPushButton("Select all");
+		auto uncheckAllButton = new QPushButton("Deselect all");
+		
+		auto getRow = [thisObject = this, this, checkAllButton, uncheckAllButton](int id) {
+			enabledChannels_.push_back(true);
+			auto hlayout = new QHBoxLayout;
+			auto channelIdLabel = new QLabel(QString("Channel %1").arg(id));
+			auto gain = new QLabel("NO DATA");
+			auto filter = new QLabel("NO DATA");
+			auto checkBox = new QCheckBox;
+			checkBox->setChecked(true);
+			connect(checkAllButton, &QPushButton::clicked, [checkBox]() {checkBox->setChecked(true); });
+			connect(uncheckAllButton, &QPushButton::clicked, [checkBox]() {checkBox->setChecked(false); });
+			auto measurement = new QLabel("THERE WAS NO MEASUREMENT");
+
+			QObject::connect(thisObject, &StevesController::measurementUpdate,
+				[id, measurement, thisObject](auto const& values) {
+					if (thisObject->enabledChannels_[id - 1])
+						if(values[id - 1])
+							measurement->setText(QString("%1V").arg(*values[id - 1], 10, 'f', 7));
+						else
+							measurement->setText("ERR");
+					else
+						measurement->setText("OFF");
+				}
+			);
+
+			QObject::connect(thisObject, &StevesController::gainsUpdate,
+				[id, gain, thisObject](auto const& values) {
+					if (values[id - 1])
+						gain->setText(GainType6132::toString(*values[id - 1]));
+					else
+						gain->setText("ERR");
+				}
+			);
+
+			QObject::connect(thisObject, &StevesController::filtersUpdate,
+				[id, filter, thisObject](auto const& values) {
+					if (values[id - 1])
+						filter->setText(FilterType6132::toString(*values[id - 1]));
+					else
+						filter->setText("ERR");
+				}
+			);
+
+			QObject::connect(checkBox, &QCheckBox::stateChanged, 
+				[thisObject, id](auto const state) {
+					thisObject->enabledChannels_[id - 1] = state == Qt::Checked;
+				}
+			);
+
+			hlayout->addWidget(channelIdLabel);
+			hlayout->addWidget(gain);
+			hlayout->addWidget(filter);
+			hlayout->addWidget(checkBox);
+			hlayout->addWidget(measurement);
+			return hlayout;
+		};
+
+		auto statusGroup = new QGroupBox("Status");
+		auto vlayout = new QVBoxLayout;
+		for (auto channel : allChannels)
+			vlayout->addLayout(getRow(channel));
+
+		hlayout = new QHBoxLayout;
+		hlayout->addWidget(checkAllButton);
+		hlayout->addWidget(uncheckAllButton);
+		vlayout->addLayout(hlayout);
+		statusGroup->setLayout(vlayout);
+
+		auto contollerGroup = new QGroupBox("Conversion Controller");
+		hlayout = new QHBoxLayout;
+		hlayout->addWidget(measureButton_);
+		hlayout->addWidget(startStopAcqButton_);
+		contollerGroup->setLayout(hlayout);
+
+		auto layout = new QVBoxLayout;
+		layout->addWidget(setterGroup_);
+		layout->addWidget(statusGroup);
+		layout->addWidget(contollerGroup);
+
+		setLayout(layout);
+	}
+};
--- main.cpp
+++ main.cpp
@@ -1,68 +1,93 @@
 #include <QApplication>
 #include <QDebug>
-#include "../WizardFramework/Common/include/Device6991/gui/TestsController.h"
-#include "../WizardFramework/Common/include/Device6991/gui/Controller6991.h"
-#include "../WizardFramework/Common/include/Device6991/gui/RegisterController.h"
-#include "../WizardFramework/Common/include/Device6991/Device6991.h"
-#include <QImage>
-
-#define MOCK // needs to be commented in real app
-
-#ifdef MOCK
-#include "../WizardFramework/Mock/include/HardwareConnectorMock.h"
-#include "../WizardFramework/Common/include/Device6991/ScpiIF6991Mock.h"
-#elif
 #include "../WizardFramework/Common/include/Device6991/HardwareConnector6991.h"
 #include "../WizardFramework/Common/include/ScpiIFVisa.h" 
-#endif
+#include "StevesController.h"
+
+void printOnScreen(QString const& msg) {
+	qDebug() << msg;
+}
 
 int main(int argc, char *argv[]) {
+	//DONT CHANGE
 	QApplication a(argc, argv);
 	QCoreApplication::setOrganizationName("Bustec");
 	QCoreApplication::setOrganizationDomain("Bustec");
 	QCoreApplication::setApplicationName("6991_TEST");
-	QString ip6991 = "127.0.0.1"; // 6991 device ip - in mock version it should be localhost
-	QString connectorName1 = "connector1";
-	QString connectorName2 = "connector2";
-#ifdef MOCK
-	auto connector1 = new HardwareConnectorMock(connectorName1); 
-	auto connector2 = new HardwareConnectorMock(connectorName2);
-	QSettings().setValue("ip"+connectorName1, ip6991);
-	QSettings().setValue("ip"+connectorName2, ip6991);
-	auto scpiIF = new ScpiIF6991Mock;
-#elif
-	auto connector2 = new HardwareConnector6991(connectorName1);
-	auto connector2 = new HardwareConnector6991(connectorName2);
-	auto scpiIF = new ScpiIFVisa;
-#endif
-	QSettings().setValue(connectorName1, ip6991);
-	QSettings().setValue(connectorName2, ip6991);
-
-	Device6991 dev("Device6111", connector1, scpiIF, 256);
-
-	auto controller6991 = new Controller6991(connector1, scpiIF);
-	auto controller6991_2 = new Controller6991(connector2, scpiIF);
-	auto resgisterControllerFrontend = new RegisterControllerFrontend(connector1, scpiIF);
-	auto resgisterController6991 = new RegisterController6991(connector1, scpiIF);
-	auto testController = new TestsController(connector1, scpiIF);
-
-	auto w = new QWidget;
-	auto layout = new QHBoxLayout;
-	auto vlayout = new QVBoxLayout;
-
-	vlayout->addWidget(resgisterController6991);
-	vlayout->addWidget(resgisterControllerFrontend);
-	QImage bustecLogo("bustec.jpg");
-	auto imageWidget = new QLabel;
-	imageWidget->setPixmap(QPixmap::fromImage(bustecLogo));
-	vlayout->addWidget(imageWidget);
-
-	layout->addWidget(controller6991);
-	layout->addLayout(vlayout);
-	w->setLayout(layout);
+	//DONT CHANGE
 	
-	controller6991_2->show();
-	w->show();
+	// CHANGE HERE
+	QString ip6991 = "192.168.168.126";		/// SET DEVICE IP HERE
+	FecIdType::Type fecId = FecIdType::_1;	/// SET FRONTEND CARD ID (SLOT) _1 OR _2 this test soft doesnt support both cards at same time (I can add second controller if you want)
+	// CHANGE HERE
 
+	//DONT CHANGE
+	QString connectorName1 = "connector";
+	QSettings settings;
+	settings.setValue("ip"+ connectorName1, ip6991);
+	auto connector1 = new HardwareConnector6991(connectorName1);
+	auto scpiIF = new ScpiIFVisa;
+	Device6991 dev("Device6132", connector1, scpiIF, 256);
+	dev.connect();
+	dev.enableScpiCommandsPrints(false);
+	Device6132 dev6132(fecId, &dev);	
+	StevesController ctrl(&dev6132);//Gui ctrl
+	ctrl.show();
+	//DONT CHANGE 
+
+	if (dev6132.isValidDev()) {			
+		{	
+			printOnScreen("6132 Successfully recognized");
+			//TEST CODE START  ----------------------------------------------------------------------------------------------
+
+			//EXAMPLE CODE START -	This code is showing gains and filters for all channels then changes them to selected values and shows again. After that it does measurement and shows results.
+			//						To check it out you need to uncomment the code section below till "EXAMPLE CODE END" line.
+			//						HINT: Select lines you want to uncomment and click CTRL+U. To comment again click CTRL+K.
+
+			////channel selection
+			//std::vector<uint32_t> channels = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+			////show gains and filters
+			//printOnScreen("========================GAINS AND FILTERS=======================");
+			//for (auto id : channels) {
+			//	if (auto gain = dev6132.gain(id); gain)
+			//		printOnScreen(QString("Channel: %1 Gain: %2").arg(id).arg(GainType6132::toString(*gain))); //show gains
+			//	if (auto filter = dev6132.filter(id); filter)
+			//		printOnScreen(QString("Channel: %1 Filter: %2").arg(id).arg(FilterType6132::toString(*filter))); //show filters
+			//}
+
+			////setting gain and filter
+			//dev6132.setGain(channels, GainType6132::_1);
+			//dev6132.setFilter(channels, FilterType6132::_10Hz);
+			//printOnScreen("========================AFTER CONF CHANGED=======================");
+
+			////show gains and filters
+			//for (auto id : channels) {
+			//	if (auto gain = dev6132.gain(id); gain)
+			//		printOnScreen(QString("Channel: %1 Gain: %2").arg(id).arg(GainType6132::toString(*gain))); //show gains
+			//	if (auto filter = dev6132.filter(id); filter)
+			//		printOnScreen(QString("Channel: %1 Filter: %2").arg(id).arg(FilterType6132::toString(*filter))); //show filters
+			//}
+
+			////measure and print
+			//printOnScreen("========================MEASUREMENT=======================");
+			//if (auto data = dev6132.measure(channels); data) {
+			//	for (auto singleChannelResult : *data)
+			//		if (singleChannelResult)
+			//			printOnScreen(QString("%1V").arg(*singleChannelResult));
+			//		else
+			//			printOnScreen(QString("NOT SELECTED OR ERROR"));
+			//	
+			//}
+			//else
+			//	printOnScreen("MEASUREMENT FAILED!");	
+
+			//EXAMPLE CODE END
+
+			//TEST CODE END ----------------------------------------------------------------------------------------------------
+		}
+	}
+	else
+		printOnScreen("6132 card not recognized or problem with connection!"); // will show up if 6132 will not be recognized on the start of the program
+	
 	return a.exec();
 }
