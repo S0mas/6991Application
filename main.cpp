e30f20aea2e9bbb52d46e86a0e19c5deaf7d332c
S0mas <sommerfeld.krzysztof@gmail.com>
2020-05-18 11:31:33 +0200

    dbg now turns on only when -dbg parameter is passed

--- main.cpp
+++ main.cpp
@@ -6,27 +6,33 @@
 #include "../WizardFramework/Common/include/Device6991/ScpiIF6991Mock.h"
 #include "../WizardFramework/Common/include/gui/Controller6132.h"
 #include "../WizardFramework/Common/include/Device6991/gui/Controller6991.h"
-#include <iostream>
+
+Q_DECLARE_METATYPE(StartTestsRequest);
+Q_DECLARE_METATYPE(Configuration6991);
+Q_DECLARE_METATYPE(FecIdType::Type);
+
 int main(int argc, char *argv[]) {
 	QApplication a(argc, argv);
 	QCoreApplication::setOrganizationName("Bustec");
 	QCoreApplication::setOrganizationDomain("Bustec");
 	QCoreApplication::setApplicationName("6991_TEST");
 
+	qRegisterMetaType<StartTestsRequest>();
+	qRegisterMetaType<Configuration6991>();
+	qRegisterMetaType<FecIdType::Type>();
+
 	QString connectorName1 = "6991";
 	auto connector1 = new HardwareConnector6991(connectorName1);
 	auto scpiIF = new ScpiIFVisa;
 	//auto connector1 = new HardwareConnectorMock(connectorName1);
 	//auto scpiIF = new ScpiIF6991Mock;
-	Device6991 dev("Device6991", connector1, scpiIF);
-	dev.enableScpiCommandsPrints(false);
 
 	bool dbgMode = false;
 	if (argc > 1 && QString(argv[1]) == QString("-dbg"))
 		dbgMode = true;
 
-	auto controller6991 = new Controller6991(connector1, scpiIF, dbgMode);
-	controller6991->show();
+	Controller6991 controller6991(std::make_unique<Device6991>("Device6991", connector1, scpiIF), dbgMode);
+	controller6991.show();
 
 	return a.exec();
 }
